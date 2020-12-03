f26deb40dfd11738ec7c478055665dca95b92b02
Brian Taylor <brian@vsoftsys.co.uk>
2020-12-03 10:41:39 +0000

    Added suggested workaround

--- TemplatedContentDialogIssue/TemplatedContentDialogIssue.Droid/Main.cs
+++ TemplatedContentDialogIssue/TemplatedContentDialogIssue.Droid/Main.cs
@@ -25,6 +25,7 @@ namespace TemplatedContentDialogIssue.Droid
 		public Application(IntPtr javaReference, JniHandleOwnership transfer)
 			: base(() => new App(), javaReference, transfer)
 		{
+			Uno.UI.FeatureConfiguration.Popup.UseNativePopup = false;
 			ConfigureUniversalImageLoader();
 		}
 
